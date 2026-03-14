3d516ee1f4468b4228f2b490b880a2647e7a62eb
Gabriel Garcia <ygabo@pm.me>
2026-03-14 15:14:20 -0500

    fix: add exception handling for invalid JWT tokens

--- backend/accounts/authentication.py
+++ backend/accounts/authentication.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 
 class CookieJWTAuthentication(JWTAuthentication):
@@ -8,5 +9,8 @@ class CookieJWTAuthentication(JWTAuthentication):
         if raw_token is None:
             return super().authenticate(request)
 
-        validated_token = self.get_validated_token(raw_token)
+        try:
+            validated_token = self.get_validated_token(raw_token)
+        except (InvalidToken, TokenError):
+            return None
         return self.get_user(validated_token), validated_token
